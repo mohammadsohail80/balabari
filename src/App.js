737fe7a09645e3bad33d8c62b175091cc9eb3a41
sohail <mohammadsohail80@yahoo.com>
2025-02-02 12:08:05 -0500

    new function added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Myhook from './hook/Myhook';
+import Mycode from './hook/Mycode';
+
 function App() {
   return (
     <div className="App">
      <Myhook />
+     <Mycode />
     </div>
   );
 }
---
+++ src/hook/Mycode.jsx
@@ -0,0 +1,7 @@
+export default function Mycode(){
+    return(
+        <div>
+            My code
+        </div>
+    )
+}
